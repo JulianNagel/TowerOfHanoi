b7706583ea14f0b9aabd17b8bf5814a58c3e0c36
jun <Julian.Nagel@ti8m.ch>
2025-01-20 09:31:07 +0100

    yurrr (initial commit)

---
+++ src/test/java/com/example/towerofhanoi/TowerOfHanoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.towerofhanoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TowerOfHanoiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
